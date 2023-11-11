b3b1ede15c86cf1e4fa0b937b5e5d81793b0c23e
Ivan Ganev <iganev@cytec.bg>
2023-11-10 17:18:03 -0700

    wip

--- src/reflection/datatypes.rs
+++ src/reflection/datatypes.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
@@ -27,9 +28,38 @@ pub enum Datatype {
     Json(String),
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParseDatatypeError;
+
 impl Default for Datatype {
     fn default() -> Self {
         Datatype::Varchar(45)
+    }
+}
+
+impl FromStr for Datatype {
+    type Err = ParseDatatypeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Datatype::try_from(s)
+    }
+}
+
+impl TryFrom<&str> for Datatype {
+    type Error = ParseDatatypeError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        //TODO
+
+        if value.starts_with("int") {
+            return Ok(Datatype::Int(10))
+        } else if value.starts_with("varchar") {
+            return Ok(Datatype::Varchar(45))
+        } else if value.starts_with("tinyint") {
+            return Ok(Datatype::Tinyint(1))
+        }
+
+        Ok(Datatype::Int(10))
     }
 }
 
