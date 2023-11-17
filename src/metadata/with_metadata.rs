dd61d32a0e63f5f399a2e84eb14a51dcc8665489
Ivan Ganev <iganev@cytec.bg>
2023-11-16 22:26:41 -0700

    wip

--- src/metadata/with_metadata.rs
+++ src/metadata/with_metadata.rs
@@ -1,10 +1,13 @@
 use std::collections::HashMap;
 
 pub trait WithMetadata {
+    /// Borrow meadata container for reading
     fn get_metadata(&self) -> &HashMap<String, String>;
 
+    /// Borrow metadata container for writing
     fn get_metadata_mut(&mut self) -> &mut HashMap<String, String>;
 
+    /// Set metadata key value pair
     fn set_meta(&mut self, meta_key: impl ToString, meta_value: impl ToString) -> &mut Self {
         self.get_metadata_mut()
             .insert(meta_key.to_string(), meta_value.to_string());
@@ -12,6 +15,7 @@ pub trait WithMetadata {
         self
     }
 
+    /// Set metadata flag
     fn set_meta_flag(&mut self, meta_flag: impl ToString) -> &mut Self {
         self.get_metadata_mut()
             .insert(meta_flag.to_string(), "1".to_string());
@@ -19,10 +23,12 @@ pub trait WithMetadata {
         self
     }
 
+    /// Check if metadata flag is set
     fn meta_flag(&self, flag: &str) -> bool {
         self.get_metadata().contains_key(flag)
     }
 
+    /// Get metadata value by key
     fn meta(&self, key: &str) -> Option<String> {
         self.get_metadata().get(key).cloned()
     }
