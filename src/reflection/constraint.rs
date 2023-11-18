f4f0ea9a3945c29ab7cb821e042ef10ee265ea7e
Ivan Ganev <iganev@cytec.bg>
2023-11-18 11:52:53 -0700

    wip

--- src/reflection/constraint.rs
+++ src/reflection/constraint.rs
@@ -25,16 +25,19 @@ pub struct Constraint {
 }
 
 impl WithMetadata for Constraint {
+    /// Borrow meadata container for reading
     fn get_metadata(&self) -> &HashMap<String, String> {
         &self.metadata
     }
 
+    /// Borrow meadata container for writing
     fn get_metadata_mut(&mut self) -> &mut HashMap<String, String> {
         &mut self.metadata
     }
 }
 
 impl Constraint {
+    /// Create a new constraint with at least one local and foreign column pair
     pub fn new(name: impl ToString, local: Rc<Column>, foreign: Rc<Column>) -> Self {
         Constraint {
             name: Rc::new(name.to_string()),
@@ -43,28 +46,34 @@ impl Constraint {
         }
     }
 
+    /// Get constraint name
     pub fn name(&self) -> Rc<String> {
         self.name.clone()
     }
 
+    /// Get local column, or local column from first pair
     pub fn local(&self) -> &Column {
         &self.key_pairs.first().unwrap().local
     }
 
+    /// Get foreign column, or foreign column from first pair
     pub fn foreign(&self) -> &Column {
         &self.key_pairs.first().unwrap().foreign
     }
 
+    /// Add a local/foreign column pair
     pub fn add_key_pair(&mut self, local: Rc<Column>, foreign: Rc<Column>) -> &mut Constraint {
         self.key_pairs.push(ConstraintKeyPair { local, foreign });
 
         self
     }
 
+    /// Get column pairs iterator
     pub fn key_pairs(&self) -> Iter<'_, ConstraintKeyPair> {
         self.key_pairs.iter()
     }
 
+    /// Get number of pairs
     pub fn key_pairs_count(&self) -> usize {
         self.key_pairs.len()
     }
