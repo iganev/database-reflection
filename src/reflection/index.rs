617134d4c0501b922efeaadad88162d443c75e1d
Ivan Ganev <iganev@cytec.bg>
2023-11-21 23:09:16 -0700

    wip

--- src/reflection/index.rs
+++ src/reflection/index.rs
@@ -11,6 +11,7 @@ pub struct Index {
 }
 
 impl Index {
+    /// Create an index
     pub fn new(name: impl ToString, column: Rc<Column>, primary: bool, unique: bool) -> Self {
         Index {
             name: Rc::new(name.to_string()),
@@ -20,18 +21,22 @@ impl Index {
         }
     }
 
+    /// Get index name
     pub fn name(&self) -> Rc<String> {
         self.name.clone()
     }
 
+    /// Get column
     pub fn column(&self) -> &Column {
         &self.column
     }
 
+    /// Get flag indicating whether the index is a primary key
     pub fn primary(&self) -> bool {
         self.primary
     }
 
+    /// Get a flag indicating whether the index is unique
     pub fn unique(&self) -> bool {
         self.unique
     }
