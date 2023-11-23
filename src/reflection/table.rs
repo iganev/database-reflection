d040dba5c03daa1553cf772b199c5ec00924bd0d
Ivan Ganev <iganev@cytec.bg>
2023-11-22 23:21:53 -0700

    wip

--- src/reflection/table.rs
+++ src/reflection/table.rs
@@ -20,16 +20,19 @@ pub struct Table {
 }
 
 impl WithMetadata for Table {
+    /// Borrow meadata container for reading
     fn get_metadata(&self) -> &HashMap<String, String> {
         &self.metadata
     }
 
+    /// Borrow meadata container for writing
     fn get_metadata_mut(&mut self) -> &mut HashMap<String, String> {
         &mut self.metadata
     }
 }
 
 impl Table {
+    /// Create a new empty table
     pub fn new(name: impl ToString) -> Table {
         Table {
             name: Rc::new(name.to_string()),
@@ -37,10 +40,12 @@ impl Table {
         }
     }
 
+    /// Get table name
     pub fn name(&self) -> Rc<String> {
         self.name.clone()
     }
 
+    /// Add a new column to the table
     pub fn set_column(&mut self, column: Column) -> &mut Table {
         if column.meta_flag(METADATA_FLAG_PRIMARY) && !self.primary_key.contains(&column.name()) {
             self.primary_key.push(column.name());
@@ -51,14 +56,17 @@ impl Table {
         self
     }
 
+    /// Find a column by name
     pub fn column(&self, key: &str) -> Option<Rc<Column>> {
         self.columns.get(&key.to_string()).cloned()
     }
 
+    /// Get columns iterator
     pub fn columns(&self) -> indexmap::map::Iter<'_, Rc<String>, Rc<Column>> {
         self.columns.iter()
     }
 
+    /// Add a new constraint
     pub fn set_constraint(&mut self, constraint: Constraint) -> &mut Table {
         self.constraints
             .insert(constraint.name(), Rc::new(constraint));
@@ -66,14 +74,17 @@ impl Table {
         self
     }
 
+    /// Find a constraint by name
     pub fn constraint(&self, key: &str) -> Option<Rc<Constraint>> {
         self.constraints.get(&key.to_string()).cloned()
     }
 
+    /// Get constraints iterator
     pub fn constraints(&self) -> std::collections::hash_map::Iter<'_, Rc<String>, Rc<Constraint>> {
         self.constraints.iter()
     }
 
+    /// Add a new index
     pub fn set_index(&mut self, index: Index) -> &mut Table {
         if index.primary() && !self.primary_key.contains(&index.column().name()) {
             self.primary_key.push(index.column().name());
@@ -84,22 +95,27 @@ impl Table {
         self
     }
 
+    /// Find an index by name
     pub fn index(&self, key: &str) -> Option<&Index> {
         self.indexes.get(&key.to_string())
     }
 
+    /// Get indexes iterator
     pub fn indexes(&self) -> indexmap::map::Iter<'_, Rc<String>, Index> {
         self.indexes.iter()
     }
 
+    /// Get number of primary keys
     pub fn primary_key_count(&self) -> usize {
         self.primary_key.len()
     }
 
+    /// Get primary key, or first primary key column name
     pub fn primary_key(&self) -> Option<Rc<String>> {
         self.primary_key.first().cloned()
     }
 
+    /// Get primary key column or first primary key column
     pub fn primary_key_column(&self) -> Option<Rc<Column>> {
         self.primary_key
             .first()
@@ -107,10 +123,12 @@ impl Table {
             .unwrap()
     }
 
+    /// Get primary keys iterator
     pub fn primary_keys(&self) -> Iter<'_, Rc<String>> {
         self.primary_key.iter()
     }
 
+    /// Get primary key columns
     pub fn primary_key_columns(&self) -> Vec<Rc<Column>> {
         self.columns
             .iter()
