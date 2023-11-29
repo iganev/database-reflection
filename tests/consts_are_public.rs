6e0e27923465277d09a0adb7b8e89a2f48a67e45
Ivan Ganev <iganev@cytec.bg>
2023-11-28 19:18:15 -0700

    wip

---
+++ tests/consts_are_public.rs
@@ -0,0 +1,16 @@
+use database_reflection::metadata::consts::*;
+
+#[test]
+fn test_consts_are_public() {
+    assert!(METADATA_CHARSET.len() > 0);
+    assert!(METADATA_COLLATION.len() > 0);
+    assert!(METADATA_ON_UPDATE.len() > 0);
+    assert!(METADATA_ON_DELETE.len() > 0);
+    assert!(METADATA_CASCADE.len() > 0);
+    assert!(METADATA_SET_NULL.len() > 0);
+
+    assert!(METADATA_FLAG_UNSIGNED.len() > 0);
+    assert!(METADATA_FLAG_NULLABLE.len() > 0);
+    assert!(METADATA_FLAG_PRIMARY.len() > 0);
+    assert!(METADATA_FLAG_AUTO_INCREMENT.len() > 0);
+}
