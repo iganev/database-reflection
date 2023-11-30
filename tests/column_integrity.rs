ac50b4c383255ee293c1fa9235b27c34125d02c0
Ivan Ganev <iganev@cytec.bg>
2023-11-30 12:33:02 -0700

    wip

--- tests/column_integrity.rs
+++ tests/column_integrity.rs
@@ -12,6 +12,7 @@ fn test_column_integrity() {
 
     assert_eq!(column_pk.name(), String::from("id").into());
     assert_eq!(column_pk.table(),String::from("test").into());
+    assert_eq!(column_pk.datatype(), &Datatype::Int(32));
     assert_eq!(column_pk.default(), None);
     assert!(column_pk.meta_flag(METADATA_FLAG_UNSIGNED));
     assert!(column_pk.meta_flag(METADATA_FLAG_PRIMARY));
@@ -24,6 +25,7 @@ fn test_column_integrity() {
 
     assert_eq!(column_vc.name(), String::from("value").into());
     assert_eq!(column_vc.table(),String::from("test").into());
+    assert_eq!(column_vc.datatype(), &Datatype::Varchar(64));
     assert_eq!(column_vc.default(), Some(DefaultValue::Value(Value::String("empty".to_string()))));
     assert_eq!(column_vc.meta(METADATA_COLLATION), Some(String::from("utf8mb4")));
     assert_eq!(column_vc.meta(METADATA_CHARSET), Some(String::from("utf8mb4_unicode_ci")));
