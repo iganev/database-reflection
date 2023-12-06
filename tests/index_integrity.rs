d1b11d9c16c27a4d81634f5b38fe9a90e83e2385
Ivan Ganev <iganev@cytec.bg>
2023-12-05 19:14:00 -0700

    wip

---
+++ tests/index_integrity.rs
@@ -0,0 +1,14 @@
+use database_reflection::reflection::{Column, Datatype, Index};
+use std::rc::Rc;
+
+#[test]
+fn test_index_integrity() {
+    let column = Rc::new(Column::new("local", "local_id", Datatype::Int(10)));
+
+    let index = Index::new("ind_local_1", column, true, false);
+
+    assert_eq!(index.name(), String::from("ind_local_1").into());
+    assert_eq!(index.column().name(), String::from("local_id").into());
+    assert!(index.primary());
+    assert!(!index.unique());
+}
