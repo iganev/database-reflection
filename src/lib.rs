e69ce56ab49c16bbb9ab0b17ab878b16a5d586f4
Ivan Ganev <iganev@cytec.bg>
2023-12-06 18:25:41 -0700

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,2 @@
 pub mod metadata;
 pub mod reflection;
-
-#[cfg(test)]
-mod tests {
-    // use crate::metadata::consts::*;
-    // use crate::metadata::WithMetadata;
-    // use crate::reflection::Column;
-    // use crate::reflection::Constraint;
-    // use crate::reflection::Database;
-    // use crate::reflection::Index;
-    // use crate::reflection::Table;
-    // use crate::reflection::{Datatype, DefaultValue};
-}
