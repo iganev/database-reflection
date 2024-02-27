5b68d9d71b425dc2943aeae50023220f618b884b
Ivan Ganev <iganev@cytec.bg>
2024-02-26 19:08:21 -0700

    unit tests

--- tests/datatypes_work.rs
+++ tests/datatypes_work.rs
@@ -1,4 +1,4 @@
-use database_reflection::reflection::{DefaultValue, ParseDatatypeError, SqlDatatype, SqlSigned};
+use database_reflection::reflection::{DefaultValue, JsonDatatype, JsonNumber, JsonString, ParseDatatypeError, SqlDatatype, SqlSigned};
 use serde_json::Value;
 use std::str::FromStr;
 
@@ -65,6 +65,10 @@ fn test_datatypes() {
         Some(32)
     );
     assert_eq!(
+        SqlDatatype::try_from("bigint(32)").ok().map(|t| (&t).try_into()).unwrap(),
+        Ok(JsonDatatype::Number(JsonNumber::BigInt))
+    );
+    assert_eq!(
         SqlDatatype::try_from("float(4,2)"),
         Ok(SqlDatatype::Float(4, 2, SqlSigned::Signed))
     );
@@ -77,8 +81,16 @@ fn test_datatypes() {
         Some(4)
     );
     assert_eq!(
+        SqlDatatype::try_from("float(4,2)").ok().map(|t| (&t).try_into()).unwrap(),
+        Ok(JsonDatatype::Number(JsonNumber::Float))
+    );
+    assert_eq!(
         SqlDatatype::try_from("double(10,2) unsigned"),
         Ok(SqlDatatype::Double(10, 2, SqlSigned::Unsigned))
+    );
+    assert_eq!(
+        SqlDatatype::try_from("double(10,2) unsigned").ok().map(|t| (&t).try_into()).unwrap(),
+        Ok(JsonDatatype::Number(JsonNumber::Float))
     );
     assert_eq!(
         SqlDatatype::try_from("double(10,2) unsigned").ok().map(|t| t.sign()).unwrap_or_default(),
@@ -102,7 +114,15 @@ fn test_datatypes() {
     );
 
     assert_eq!(SqlDatatype::try_from("date"), Ok(SqlDatatype::Date));
+    assert_eq!(
+        SqlDatatype::try_from("date").ok().map(|t| (&t).try_into()).unwrap(),
+        Ok(JsonDatatype::String(JsonString::Date, Some(10)))
+    );
     assert_eq!(SqlDatatype::try_from("time"), Ok(SqlDatatype::Time));
+    assert_eq!(
+        SqlDatatype::try_from("time").ok().map(|t| (&t).try_into()).unwrap(),
+        Ok(JsonDatatype::String(JsonString::Time, Some(8)))
+    );
     assert_eq!(SqlDatatype::try_from("datetime"), Ok(SqlDatatype::Datetime));
     assert_eq!(
         SqlDatatype::try_from("timestamp"),
@@ -122,6 +142,10 @@ fn test_datatypes() {
     );
     assert_eq!(SqlDatatype::try_from("varchar(64)").ok().map(|t| t.len()).unwrap_or_default(), Some(64));
     assert_eq!(
+        SqlDatatype::try_from("varchar(64)").ok().map(|t| (&t).try_into()).unwrap(),
+        Ok(JsonDatatype::String(JsonString::String, Some(64)))
+    );
+    assert_eq!(
         SqlDatatype::try_from("text(1024)"),
         Ok(SqlDatatype::Text(1024))
     );
@@ -139,7 +163,10 @@ fn test_datatypes() {
         Ok(SqlDatatype::Varbinary(32))
     );
     assert_eq!(SqlDatatype::try_from("varbinary(32)").ok().map(|t| t.len()).unwrap_or_default(), Some(32));
-
+    assert_eq!(
+        SqlDatatype::try_from("varbinary(32)").ok().map(|t| (&t).try_into()).unwrap(),
+        Ok(JsonDatatype::String(JsonString::String, Some(32)))
+    );
     assert_eq!(
         SqlDatatype::try_from(r#"enum("one","two","three")"#),
         Ok(SqlDatatype::Enum(vec![
@@ -149,6 +176,10 @@ fn test_datatypes() {
         ]))
     );
     assert_eq!(SqlDatatype::try_from(r#"enum("one","two","three")"#).ok().map(|t| t.len()).unwrap_or_default(), Some(3));
+    assert_eq!(
+        SqlDatatype::try_from(r#"enum("one","two","three")"#).ok().map(|t| (&t).try_into()).unwrap(),
+        Ok(JsonDatatype::Array(vec!["one".to_string(), "two".to_string(), "three".to_string()]))
+    );
     assert_eq!(
         SqlDatatype::try_from(r#"set("this","that")"#),
         Ok(SqlDatatype::Set(vec![
